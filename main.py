7bf62a6fd5e9870141ccf183cf0b97005a19eb7f
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-01-04 21:14:38 +0200

    some code

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
-running=True
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Space Shooter")
+icon = pygame.image.load("images/icon.png")
+pygame.display.set_icon(icon)
+
+background = pygame.image.load("images/bg.png")
+player_image = pygame.image.load("images/arcade.png")
+spaceshipX=370
+spaceshipY=480
+
+def player():
+    screen.blit(player_image, (spaceshipX, spaceshipY))
+
+running = True
 while running:
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            running=False
+        if event.type == pygame.QUIT:
+            running = False
+    spaceshipX+=1
+    player()
+    pygame.display.update()
