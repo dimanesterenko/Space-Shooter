049fbcdc3525011723598afab30d9813fda05419
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-01-06 01:36:24 +0200

    add bullets basics

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ pygame.display.set_icon(icon)
 background = pygame.image.load("images/bg.png")
 player_image = pygame.image.load("images/arcade.png")
 enemy_image = pygame.image.load("images/enemy.png")
+bullet_image = pygame.image.load("images/bullet.png")
 spaceshipX=370
 spaceshipY=480
 changeX=0
 enemyX=random.randint(0,738)
 enemyY=random.randint(30,150)
 enemyspeedX=3
+enemyspeedY=10
+
+check=False
+bulletX=370
+bulletY=480
 
 running = True
 while running:
@@ -30,6 +36,9 @@ while running:
                 changeX = -5
             if event.key == pygame.K_RIGHT:
                 changeX = 5
+            if event.key == pygame.K_SPACE:
+                check=True
+
         if event.type == pygame.KEYUP:
             changeX=0
     spaceshipX+=changeX
@@ -41,12 +50,15 @@ while running:
 
     if enemyX<=0:
         enemyspeedX=3
+        enemyY+=enemyspeedY
     if enemyX>=738:
         enemyspeedX=-3
+        enemyY+=enemyspeedY
     enemyX+=enemyspeedX
-
-
+    if check is True:
+        screen.blit(bullet_image, (bulletX, bulletY))
 
     screen.blit(player_image, (spaceshipX, spaceshipY))
     screen.blit(enemy_image, (enemyX, enemyY))
+
     pygame.display.update()
