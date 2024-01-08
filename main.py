e0007b9f153a946038309f26c2bfcbccbe6a3482
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-01-08 20:48:38 +0200

    add sounds

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import pygame
 import random
 import math
+from pygame import mixer
+
+mixer.init()
 pygame.init()
+
+mixer.music.load("audio/background.wav")
+mixer.music.play(-1)
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Space Shooter")
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
 
+
 background = pygame.image.load("images/bg.png")
 player_image = pygame.image.load("images/arcade.png")
-enemy_image = pygame.image.load("images/enemy.png")
+
 bullet_image = pygame.image.load("images/bullet.png")
 spaceshipX=370
 spaceshipY=480
 changeX=0
-enemyX=random.randint(0,738)
-enemyY=random.randint(30,150)
-enemyspeedX=3
-enemyspeedY=40
+
+enemy_image=[]
+enemyX=[]
+enemyY=[]
+enemyspeedX=[]
+enemyspeedY=[]
+
+num_of_enemies=10
+for x in range(num_of_enemies):
+    enemy_image.append(pygame.image.load("images/enemy.png"))
+    enemyX.append(random.randint(0,738))
+    enemyY.append(random.randint(30,150))
+    enemyspeedX.append(-1)
+    enemyspeedY.append(40)
 
 check = False
 bulletX = 386
@@ -41,9 +58,7 @@ def gameover():
 
 
 def collision():
-    distance = math.sqrt(math.pow(bulletX-enemyX,2)+math.pow(bulletY-enemyY,2))
-    if distance<27:
-        return True
+    pass
 
 
 running = True
@@ -61,6 +76,8 @@ while running:
                 changeX = 5
             if event.key == pygame.K_SPACE:
                 if check is False:
+                    laserSound=mixer.Sound("audio/laser.wav")
+                    laserSound.play()
                     check = True
                     bulletX = spaceshipX + 16
         if event.type == pygame.KEYUP:
@@ -70,15 +87,30 @@ while running:
         spaceshipX=3
     elif spaceshipX>=738:
         spaceshipX=738
+    for i in range(num_of_enemies):
+        if enemyY[i] > 420:
+            for j in range(num_of_enemies):
+                enemyY[i] = 2000
+            gameover()
+            break
+        enemyX[i]+=enemyspeedX[i]
+        if enemyX[i]<=0:
+            enemyspeedX[i]=3
+            enemyY[i]+=enemyspeedY[i]
+        if enemyX[i]>=738:
+            enemyspeedX[i]=-3
+            enemyY[i]+=enemyspeedY[i]
 
-    enemyX+=enemyspeedX
-
-    if enemyX<=0:
-        enemyspeedX=3
-        enemyY+=enemyspeedY
-    if enemyX>=738:
-        enemyspeedX=-3
-        enemyY+=enemyspeedY
+        distance = math.sqrt(math.pow(bulletX - enemyX[i], 2) + math.pow(bulletY - enemyY[i], 2))
+        if distance < 27:
+            explSound=mixer.Sound("audio/explosion.wav")
+            explSound.play()
+            bulletY = 480
+            check = False
+            score += 1
+            enemyX[i] = random.randint(0, 736)
+            enemyY[i] = random.randint(30, 150)
+        screen.blit(enemy_image[i], (enemyX[i], enemyY[i]))
 
     if bulletY<=0:
         bulletY=490
@@ -88,19 +120,9 @@ while running:
         screen.blit(bullet_image, (bulletX, bulletY))
         bulletY-=10
 
-    if enemyY>420:
-        enemyY=2000
-        gameover()
 
 
-    collision_occured = collision()
-    if collision_occured:
-        bulletY=480
-        check=False
-        score+=1
-        enemyX=random.randint(0,736)
-        enemyY=random.randint(30,150)
+
     screen.blit(player_image, (spaceshipX, spaceshipY))
-    screen.blit(enemy_image, (enemyX, enemyY))
     score_text()
     pygame.display.update()
